9d951c358f497c52a5a6f224643764eafbfa0573
sid ufsj <sidney.fejr@aluno.ufsj.edu.br>
2023-09-14 09:16:31 -0300

    criando o projeto para ods

---
+++ sgaq/src/test/java/br/edu/ufsj/comp/sgaq/sgaq/SgaqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufsj.comp.sgaq.sgaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
