18a976295afeaa292486d0b8f1c449fd70c6b248
lsouz44 <lele.luizsouza@hotmail.com>
2023-11-14 08:52:01 -0300

    Teste 14

--- sgaq/src/main/java/br/edu/ufsj/dcefs/sgaq/SgaqApplication.java
+++ sgaq/src/main/java/br/edu/ufsj/dcefs/sgaq/SgaqApplication.java
@@ -2,14 +2,12 @@ package br.edu.ufsj.dcefs.sgaq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SgaqApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SgaqApplication.class, args );
+        SpringApplication.run(SgaqApplication.class, args);
     }
 
 
